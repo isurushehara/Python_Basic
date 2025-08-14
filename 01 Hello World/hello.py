5f937b7599796d4523d91d742ae0e6295796203e
Isuru Shehara <isurushehara@gmail.com>
2025-08-15 00:48:01 +0530

    Updated lesson 1 and Added lesson 2.1

--- 01 Hello World/hello.py
+++ 01 Hello World/hello.py
@@ -1,1 +1,3 @@
+# This is a simple Python script to print "Hello World!"
+# It uses the print function to display the message.
 print("Hello World!")
---
+++ 02 Types, Variables, Operators and Type Conversion/data_type.py
@@ -0,0 +1,19 @@
+# Data Types in Python
+print("Data Types in Python")
+
+print("Integer:", 42)
+print("Float:", 3.14)
+print("String:", "Hi")
+print('String with single quotes:', 'Hi')
+print("Boolean:", True)
+print("Complex Number:", 1 + 2j)
+
+#Escape Characters
+print("\nEscape Characters")
+print("One: 1 \nTwo: 2 \nThree: 3")
+print("One\t: 1")
+print("Two\t: 2")
+print("Three\t: 3")
+print("Backslash: \\ -> (eg: C:\\Users\\Name)")
+print("Double Quote: \" -> (eg: \"Hello\")")
+
