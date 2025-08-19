9cae499d3efc7862e8f8c6dcb21d44af73c2498d
Isuru Shehara <isurushehara@gmail.com>
2025-08-19 22:49:34 +0530

    Add lesson 2.2 variables section

---
+++ 02 Types, Variables, Operators and Type Conversion/variables_operators.py
@@ -0,0 +1,16 @@
+# Variables 
+# No need to add type of the data, just assign the value with a proper variable name
+# variable_name = value
+# Example of variable assignment in Python
+
+integerNumber = 42
+print("Integer:", integerNumber)
+
+floatNumber = 3.14
+print("Float:", floatNumber)
+
+stringName = "Isuru"
+print("String:", stringName)
+
+booleanValue = True
+print("Boolean:", booleanValue)
