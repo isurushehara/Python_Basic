ccf7392fa41a5dac2a6b08e43375a664ae4b5a6a
Isuru Shehara <isurushehara@gmail.com>
2025-08-20 20:16:01 +0530

    Modify the 1 and 2.1 lessons

--- 01 Hello World/hello.py
+++ 01 Hello World/hello.py
@@ -1,3 +1,12 @@
 # This is a simple Python script to print "Hello World!"
 # It uses the print function to display the message.
 print("Hello World!")
+
+#Escape Characters
+print("\nEscape Characters")
+print("One: 1 \nTwo: 2 \nThree: 3")
+print("One\t: 1")
+print("Two\t: 2")
+print("Three\t: 3")
+print("Backslash: \\ -> (eg: C:\\Users\\Name)")
+print("Double Quote: \" -> (eg: \"Hello\")")
--- 02 Types, Variables, Operators and Type Conversion/data_type.py
+++ 02 Types, Variables, Operators and Type Conversion/data_type.py
@@ -8,12 +8,4 @@ print('String with single quotes:', 'Hi')
 print("Boolean:", True)
 print("Complex Number:", 1 + 2j)
 
-#Escape Characters
-print("\nEscape Characters")
-print("One: 1 \nTwo: 2 \nThree: 3")
-print("One\t: 1")
-print("Two\t: 2")
-print("Three\t: 3")
-print("Backslash: \\ -> (eg: C:\\Users\\Name)")
-print("Double Quote: \" -> (eg: \"Hello\")")
 
